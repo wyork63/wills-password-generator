4622898fbd93f720c25ceed5f40cd5529fa70d07
wyork <w.york63@gmail.com>
2021-12-02 22:57:28 -0500

    added in math to pick random variable

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,51 @@
-//Opening welcome
-alert("Welcome to Will's password generator. Click the red button to get started")
-
-// Assignment code here
-function generatePassword() {
-
-  // first prompt 
-  var lengthOfPassword = window.prompt("Type in how long you would like your password to be (between 8 and 128 characters)");
-  // if password is not between 8 and 128 it returns to prompt again 
-    if (lengthOfPassword < 8 || lengthOfPassword > 128 || lengthOfPassword == ""); {
-      lengthOfPassword = window.prompt("Your input is invalid. Make sure your legth is between 8 and 128");
-      return lengthOfPassword ()
-    }
-
-    
-  // lowercase, uppercase, numbers, special characters   
-  var yesLowerCase = window.confirm("Would you like to include LOWERCASE characters in your password. Click OK for yesr or CANCEL for no");
-  var yesUpperCase = window.confirm("Would you like to include UPPERCASE characters in your password. Click OK for yesr or CANCEL for no");
-  var yesNumbers = window.confirm("Would you like to include NUMBERS in your password. Click OK for yesr or CANCEL for no");
-  var yesSpecialCharacters = window.confirm("Would you like to include SPECIAL CHARACTERS in your password. Click OK for yesr or CANCEL for no");
-    
-
-// Prompt the user for password criteria
-
- 
-  var passwordLength
-
-}
-
-
-
-// validate the input 
-// generate password based on criteria 
-
-
-// once all steps are completed display the password on the page ie return the password 
-  
-  
-  // return "heres your password"; 
-
-
 // variables to pick from
 
-var specialCharacter = [ ' ', '!', '"','#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.','/',':',';','<','=','>','?','@','[','^','_','`','{','|','}','~' ]
+var specialCharacter = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.','/',':',';','<','=','>','?','@','[','^','_','`','{','|','}','~']
 
-var number = [ '0','1','2','3','4','5','6','7','8','9']
+var number = ['0','1','2','3','4','5','6','7','8','9']
 
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+//Opening welcome
+alert("Welcome to Will's password generator. Click the red button to get started.");
+
+// Assignment code here
+function generatePassword() {{
+
+  // first prompt 
+  var lengthOfPassword = Number(window.prompt("Type in how long you would like your password to be (between 8 and 128 characters)"));
+  // if password is not between 8 and 128 it returns to prompt again 
+    while(lengthOfPassword < 8 || lengthOfPassword > 128) {
+      lengthOfPassword = Number(window.prompt("Your input is invalid. Make sure your legth is between 8 and 128"));  
+    }
+  }
+
+  //function that says yes or not to lowercase letters
+  var yesLowerCase = window.confirm("Would you like to include LOWERCASE characters in your password? Click OK for yes or CANCEL for no");
+    if (yesLowerCase) {
+
+    }
+  // function that says yes or no to upper case letters
+  var yesUpperCase = window.confirm("Would you like to include UPPERCASE characters in your password? Click OK for yes or CANCEL for no");
+
+
+  // funciton that says yes or no to numbers
+  var yesNumber = window.confirm("Would you like to include NUMBERS in your password? Click OK for yes or CANCEL for no");
+  
+
+  // function that says yes or no to special charcaters
+  var yesSpecialCharacter = window.confirm("Would you like to include SPECIAL CHARACTERS in your password? Click OK for yes or CANCEL for no");
+    
+
+  //function that gathers responses from prior answer 
+
+  // for loop that takes these answers - randomly grabs string of variables - then generates it in the box on screen 
+  var randomAnswer = specialCharacter[Math.floor(Math.random() * specialCharacter.length)];
+  console.log(randomAnswer);
+  }
+
 
 //final password
 var finalPassword = "";
