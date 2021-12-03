def024a1d9e95da4beb659fd1873b9c4370accde
wyork <w.york63@gmail.com>
2021-12-03 17:11:47 -0500

    changed final password to +=

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,8 @@
 // variables to pick from
+//using these variables includes the commas 
 // var specialCharacter = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.','/',':',';','<','=','>','?','@','[','^','_','`','{','|','}','~']
-
 // var number = ['0','1','2','3','4','5','6','7','8','9']
-
 // var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-
 // var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 var specialCharacter = " !\"#$%&`'()*+,-.:'<=>?@[^_{}|`]"
@@ -84,16 +82,12 @@ function generatePassword() {{
   var finalPassword = "";
   // var randomanswer can be replaced with specialcharacter
   for(var i = 0; i < lengthOfPassword; i++) {
-   finalPassword = firstPassword.charAt(Math.floor(Math.random() * firstPassword.length));
+   finalPassword += firstPassword.charAt(Math.floor(Math.random() * firstPassword.length));
   }
+  //final password
     return finalPassword; 
 
-  console.log(randomAnswer);
   }
-
-
-//final password
-
 
 // GIVEN CODE BELOW
 
