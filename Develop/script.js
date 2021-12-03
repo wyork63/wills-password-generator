c85b3b98206f23fd0b41a82cc4bea66df32c8940
wyork <w.york63@gmail.com>
2021-12-02 21:28:28 -0500

    length of password question

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,41 @@
+//Opening welcome
+alert("Welcome to Will's password generator. Click the red button to get started")
+
 // Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 function generatePassword() {
-  window.prompt("Thank you for clicking the button");
+
+  // first prompt 
+  var lengthOfPassword = window.prompt("Type in how long you would like your password to be (between 8 and 128 characters)");
+  // if password is not between 8 and 128 it returns to prompt again 
+    if (lengthOfPassword < 8 || lengthOfPassword > 128 || lengthOfPassword == ""); {
+      lengthOfPassword = window.prompt("Your input is invalid. Make sure your legth is between 8 and 128");
+      return lengthOfPassword ()
+    }
+
+    
 
 
+  // lowercase, uppercase, numbers, special characters   
+  var yesLowerCase = window.confirm("Would you like to include LOWERCASE characters in your password. Click OK for yesr or CANCEL for no");
+  var yesUpperCase = window.confirm("Would you like to include UPPERCASE characters in your password. Click OK for yesr or CANCEL for no");
+  var yesNumbers = window.confirm("Would you like to include NUMBERS in your password. Click OK for yesr or CANCEL for no");
+  var yesSpecialCharacters = window.confirm("Would you like to include SPECIAL CHARACTERS in your password. Click OK for yesr or CANCEL for no");
+    
+
+  var finalPassword = "";
+  for (var i = 0; i < lengthOfPassword; i++) {
+    finalPassword 
+  }
 
 // Prompt the user for password criteria
-//    password length is between 8 and 128 characters
-//      cant be 1-7 or 129-infinity 
-//    lowercase, uppercase, numbers, special characters
+
+ 
+  var passwordLength
+
+}
+
+
+
 // validate the input 
 // generate password based on criteria 
 
@@ -20,16 +43,8 @@ function generatePassword() {
 // once all steps are completed display the password on the page ie return the password 
   
   
-  return "heres your password"; 
-}
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // return "heres your password"; 
 
-  passwordText.value = password;
-
-}
 
 // variables to pick from
 
@@ -41,6 +56,22 @@ var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+//final password
+var finalPassword = "";
+
+
+// GIVEN CODE BELOW
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
